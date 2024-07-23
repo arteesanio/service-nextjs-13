67e6fef12ec2e05ec09051b8d51426324c129c02
Abraham Duno <abraham.d@servicepad.com>
2024-07-23 19:19:22 -0400

    responsive 4

--- src/dom/landing/AboutUsRecovery.tsx
+++ src/dom/landing/AboutUsRecovery.tsx
@@ -4,18 +4,23 @@ export const AboutUsRecovery = () => {
 <div className='h-min-90vh pt-100 flex-col w-100 '>
           <img src='/sectionspng/gridimage.png' className='block w-100 pos-abs  opaci-50' />
           <div className='w-max-1080px w-100 flex-col flex-align-start z-5 '>
-            <div className='tx-xl tx-altfont-1 tx-bold-8 w-max-700px px-4'>Tecnología de la información</div>
-            <div className='tx-xl tx-altfont-1 tx-bold-8 w-max-700px px-4'>iRecovery Data Panamá</div>
+          <div className='Q_sm_x tx-xl tx-altfont-1 tx-bold-8 w-max-700px px-4'>Tecnología de la información</div>
+          <div className='Q_xs tx-lg tx-altfont-1 tx-bold-8 w-max-700px px-4'>iRecovery Data Panamá</div>
+          <div className='Q_xs tx-lx tx-altfont-1 tx-bold-8 w-max-700px px-4'>Tecnología de la información</div>
+            <div className='Q_sm_x tx-xl tx-altfont-1 tx-bold-8 w-max-700px px-4'>iRecovery Data Panamá</div>
             <div className='py-4'></div>
             <div className='tx-altfont-1 w-max-600px py-1 w-100 '>
-              <div className='px-4'>El análisis forense de datos es esencial para cualquier empresa que busque protegerse contra el fraude y otros delitos informáticos.</div>
+              <div className='px-4'>El análisis forense de datos es esencial para cualquier empresa que busque protegerse contra el fraude<div className="Q_sm_x"> y otros delitos informáticos</div>.</div>
             </div>
             <div className='tx-altfont-1 w-max-600px py-1 w-100 '>
               <div className='px-4'>Datos virtuales podrán ser presentados como pruebas electrónicas ante la justicia.</div>
             </div>
+            <div className="Q_xs py-8 pos-rel">
+            <div  className='pos-abs pa-100 ' style={{filter: "blur(80px)", background: "#06224377"}}></div>
+            </div>
             <div className='px-4 flex-col flex-align-start'>
               <div className='tx-altfont-1 tx-lg mt-4 py-2 bord-r-100 px-4  border-white opaci-chov--50 tx-bold-2'>
-                <div className=''>Contacta nuestro equipo de expertos y recupera datos →</div>
+                <div className='Q_xs_py-4'>Contacta nuestro equipo de expertos y recupera datos →</div>
               </div>
               <div className='tx-altfont-1 tx-lg mt-4 py-2 bord-r-100 px-4  border-white opaci-chov--50 tx-bold-2'>
                 Analisis Gratis →
