3cfed0547e803dbeda39589837ae8b5f0aa74f5f
Abraham Duno <abraham.d@servicepad.com>
2024-07-28 02:42:44 -0400

    up

--- src/dom/FooterSection.tsx
+++ src/dom/FooterSection.tsx
@@ -1,107 +1,69 @@
-import { FaWhatsapp, FaInstagram, FaTiktok, FaGlasses, FaSun, FaMoon, FaQuestionCircle, FaPhoneAlt, FaBuilding, FaCity, FaMap, FaHandshake } from "react-icons/fa";
-import { FaHandBackFist, FaEarListen, FaEarDeaf, FaMapLocation, FaShop } from "react-icons/fa6";
-import { TbShoe } from "react-icons/tb";
-
-
+import { FaTwitter, FaTelegram, FaYoutube, FaTiktok, FaExchangeAlt, FaFileAlt, FaChartLine, FaAddressCard } from "react-icons/fa";
 
 export const FooterSection = () => {
-  return (<>
-
-
-    <div className=' w-100  flex-col py-8 tx-white' style={{ background: "#08090D" }}>
-      <div className=' w-100  w-max-1080px'>
-        {/* <h1 className='tx-altfont-1 tx-bold-2 mb-4 flex-center flex-justify-start'>
-          
-          
-          <div className=" tx-altfont-1">iRecovery</div>
-        </h1> */}
+  return (
+    <div className='w-100 flex-col py-8 tx-black' style={{ background: "#DCDCDC" }}>
+      <div className='w-100 w-max-1080px'>
         <div className='Q_xs_flex-col flex-wrap flex-align-start flex-justify-around'>
           <div className='mb-8 flex-col px-4 flex-align-start tx-altfont-1'>
-            <h2 className='tx-altfont-1 tx-bold-1'>Servicios:</h2>
+            <h2 className='tx-altfont-1 tx-bold-1'>Important Links:</h2>
             <div className='flex-col flex-align-start tx-lg'>
-              {/* <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://twitter.com/mileisolcoin">
-          <FaTwitter /> Twitter / X
-        </a> */}
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://t.me/mileisolcoin/">
-                Recuperación de Datos
+              <h3>Social Media:</h3>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://twitter.com/your_handle">
+                <FaTwitter /> Twitter / X
               </a>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://www.youtube.com/@JavierMileiSolana">
-                Análisis Forense
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://t.me/your_channel">
+                <FaTelegram /> Telegram
               </a>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://tiktok.com/@mileisolcoin">
-                Eliminación Segura
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://youtube.com/your_channel">
+                <FaYoutube /> Youtube
               </a>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://tiktok.com/@mileisolcoin">
-                Penetration Test
-              </a>
-
-              {/* <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://www.tiktok.com/@mileisolcoin">
-          <FaTiktok /> TikTok
-        </a> */}
-              {/* <a className='opaci-chov--50 tx-black nodeco opaci-50 pa-2 px-4' >TikTok (Soon)</a>
-        <a className='opaci-chov--50 tx-black nodeco opaci-50 pa-2 px-4' >Youtube (Soon)</a> */}
-            </div>
-          </div>
-
-          <div className='mb-8 flex-col px-4 flex-align-start tx-altfont-1'>
-            <h2 className='tx-altfont-1 tx-bold-1'>Partners:</h2>
-            <div className='flex-col flex-align-start tx-lg'>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://jup.ag/swap/SOL-MILEI">
-                <FaShop /> Franquicias
-              </a>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://www.coingecko.com/en/coins/milei-solana">
-                <FaHandshake /> Conviertete en Socio
-              </a>
-              <hr className="opaci-10 w-100" />
-
-              
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://tiktok.com/@mileisolcoin">
-                <FaQuestionCircle /> FAQ
-              </a>
-              <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://tiktok.com/@mileisolcoin">
-                <FaPhoneAlt /> Enviar un ticket
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://tiktok.com/@your_handle">
+                <FaTiktok /> TikTok
               </a>
             </div>
           </div>
 
           <div className='mb-8 flex-col px-4 flex-align-start tx-altfont-1'>
-            <h2 className='tx-altfont-1 tx-bold-1 py-3'></h2>
             <div className='flex-col flex-align-start tx-lg'>
-              <a className='  nodeco pa-2 px-4' 
-                style={{color: "#223344"}}
-              >
-                <FaMapLocation /> Panama City
+              <h3>Token:</h3>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://exchange.example.com">
+                <FaExchangeAlt /> Buy from exchange
               </a>
-              <a className='  nodeco pa-2 px-4' 
-                style={{color: "#223344"}}
-              >
-                <FaMap /> Costa del Este, Av La Rotonda
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://coingecko.com/your_token">
+                <FaFileAlt /> Coingecko
               </a>
-              <a className='  nodeco pa-2 px-4 tx-md' 
-                style={{color: "#223344"}}
-              >
-                <FaBuilding /> Edificio The  Prime Time Business Tower
+              <span className='opaci-50 pa-2 px-4'>Coinmarketcap (Soon)</span>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://chart.example.com">
+                <FaChartLine /> Chart
               </a>
-              {/* <a className='opaci-chov--75 tx-white opaci-50  nodeco pa-2 px-4' target='_blank' href="https://github.com/SrMessiETH">
-          <FaGithub /> Github
-        </a> */}
+            </div>
+          </div>
+
+          <div className='mb-8 flex-col px-4 flex-align-start tx-altfont-1'>
+            <div className='flex-col flex-align-start tx-lg'>
+              <h3>Other Links:</h3>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://contract.example.com">
+                <FaAddressCard /> Contract Address
+              </a>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://whitepaper.example.com">
+                <FaFileAlt /> Whitepaper
+              </a>
+              <a className='opaci-chov--75 tx-black opaci-50 nodeco pa-2 px-4' target='_blank' href="https://partnersoftware.example.com">
+                <FaChartLine /> Partner Software
+              </a>
             </div>
           </div>
         </div>
-        
+
         <div className='tx-altfont-1 tx-bold-2 mb-4 flex flex-justify-between Q_xs_flex-col'>
-          {/* Important Links: */}
-          
           <div className="tx-lx tx-altfont-1 flex-center">
-          <img src="/sectionspng/logo.png" width={32} style={{
-          }} />
-          <div className="opaci-75 pl-3">iRecovery</div>
+            <img src="/img/android-icon-48x48.png" width={32} style={{}} />
+            <div className="opaci-75 pl-3">Your Company Name</div>
           </div>
           <div className="opaci-25 tx-altfont-1">Copyright © 2024</div>
         </div>
       </div>
     </div>
-
-
-  </>);
+  );
 };
